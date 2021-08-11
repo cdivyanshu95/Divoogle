8b172d9cc94f669c96f04bd23e3b607df6aed156
Divyanshu <cdivyanshu95@gmail.com>
2021-08-11 20:11:30 +0530

    Initial com

---
+++ .next/static/webpack/webpack.8a773a8aa411994b4019.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "156a89d7296f82fecbc0"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay44YTc3M2E4YWE0MTE5OTRiNDAxOS5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjE1NmE4OWQ3Mjk2ZjgyZmVjYmMwXCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
